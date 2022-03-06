b40ec5384d70e114301c74504b39a8ed026001b2
Clemens S. Heithecker <clemens@heithecker.dev>
2022-03-06 15:22:37 +0100

    Add social media redirects
    Redirect clemensheithecker.com/github, /linkedin, and /twitter to the
    corresponding profiles.

--- next.config.js
+++ next.config.js
@@ -4,4 +4,23 @@ module.exports = {
     locales: ["en-US"],
     defaultLocale: "en-US",
   },
+  redirects: async () => {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/clemensheithecker/",
+        permanent: true,
+      },
+      {
+        source: "/linkedin",
+        destination: "https://www.linkedin.com/in/clemensheithecker/",
+        permanent: true,
+      },
+      {
+        source: "/twitter",
+        destination: "https://twitter.com/cheithecker",
+        permanent: true,
+      },
+    ];
+  },
 };
