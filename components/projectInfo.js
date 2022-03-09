4b1754f1eecf0cbbefdd3107f4dd0d706fe2476a
Clemens S. Heithecker <clemens@heithecker.dev>
2022-03-10 00:42:27 +0100

    Fix long links from not wrapping

--- components/projectInfo.js
+++ components/projectInfo.js
@@ -4,7 +4,7 @@ export default function ProjectInfo({ description, tags, link, gradient }) {
       href={link}
       target="_blank"
       rel="noopener noreferrer"
-      className="mb-4 block w-fit rounded-lg bg-gray-100 px-3 py-2 text-blue-600 no-underline hover:underline hover:decoration-2 hover:underline-offset-2 dark:bg-gray-900 dark:text-blue-500"
+      className="mb-4 block w-fit break-words rounded-lg bg-gray-100 px-3 py-2 text-blue-600 no-underline hover:underline hover:decoration-2 hover:underline-offset-2 dark:bg-gray-900 dark:text-blue-500"
     >
       {link.replace("https://", "")}
     </a>
