be26545430bcccc0059c1c39eefbd2be379a9480
Clemens S. Heithecker <clemens@heithecker.dev>
2022-03-09 12:12:30 +0100

    Add projects overview section to index page

---
+++ components/ProjectCard.js
@@ -0,0 +1,25 @@
+export default function Navitem({ title, description, tags, link, gradient }) {
+  return (
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`transform bg-gradient-to-r ${gradient} p-1 transition-all hover:scale-[1.01]`}
+    >
+      <div className="h-full bg-white p-4">
+        <h3 className="mb-2 text-lg font-semibold">{title}</h3>
+        <p className="mb-4">{description}</p>
+        <ul className="flex space-x-3">
+          {tags.map((tag, index) => (
+            <li
+              key={index}
+              className="rounded-lg bg-gray-100 px-2 py-1 text-sm text-gray-600"
+            >
+              {tag}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </a>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
 import Head from "next/head";
 import Image from "next/image";
 
+import ProjectCard from "../components/ProjectCard";
+
 import profilePicture from "../public/images/profile.jpg";
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const res = await fetch(
+    process.env.NODE_ENV === "production"
+      ? "https://clemensheithecker.com/api/projects"
+      : "http://localhost:3000/api/projects"
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      projects: data,
+    },
+  };
+};
+
+export default function Home({ projects }) {
   return (
     <>
       <Head>
@@ -20,71 +37,104 @@ export default function Home() {
         <meta property="og:url" content="https://clemensheithecker.com" />
         <meta property="og:type" content="website" />
       </Head>
-      <main className="mx-auto flex max-w-screen-lg items-center justify-center px-5 py-5 sm:min-h-[calc(100vh-68px)] sm:py-8 sm:px-11">
-        <div className="sm:grid sm:grid-cols-12 sm:grid-rows-1 sm:gap-6">
-          <div className="flex items-center justify-center sm:col-span-6 md:col-span-7">
-            <div>
-              <h2 className="mb-4 font-serif text-2xl font-bold">Hello,</h2>
-              <p className="mb-2">
-                I am Clemens&mdash;a data and programming enthusiast ready to
-                put my data wrangling, visualization, and modeling skills to
-                work. My fascination with <em>data exploration</em>,{" "}
-                <em>regression analyis</em>, and <em>machine learning</em>{" "}
-                inspire me to learn something new every day.
-              </p>
-              <p className="mb-2">
-                Currently, I am finalizing my bachelor of science degree in
-                economics as an international student at{" "}
-                <a
-                  href="https://www.tilburguniversity.edu/education/bachelors-programs/economics"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="hover:underline hover:decoration-gray-300 hover:decoration-2 hover:underline-offset-2"
-                >
-                  Tilburg University
-                </a>
-                .
-              </p>
-              <p className="mb-10">
-                Follow me on{" "}
-                <a
-                  href="https://github.com/clemensheithecker"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="hover:underline hover:decoration-gray-300 hover:decoration-2 hover:underline-offset-2"
-                >
-                  GitHub
-                </a>
-                ,{" "}
-                <a
-                  href="https://www.linkedin.com/in/clemensheithecker"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="hover:underline hover:decoration-gray-300 hover:decoration-2 hover:underline-offset-2"
-                >
-                  LinkedIn
-                </a>{" "}
-                and{" "}
-                <a
-                  href="https://twitter.com/cheithecker"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="hover:underline hover:decoration-gray-300 hover:decoration-2 hover:underline-offset-2"
-                >
-                  Twitter
-                </a>
-                .
-              </p>
+      <main className="mx-auto max-w-screen-lg">
+        <div className="flex items-center justify-center px-5 py-5 sm:min-h-[calc(75vh-68px)] sm:py-8 sm:px-11">
+          <div className="sm:grid sm:grid-cols-12 sm:grid-rows-1 sm:gap-6">
+            <div className="flex items-center justify-center sm:col-span-6 md:col-span-7">
+              <div>
+                <h2 className="mb-4 font-serif text-2xl font-bold">Hello,</h2>
+                <p className="mb-2">
+                  I am Clemens&mdash;a data and programming enthusiast ready to
+                  put my data wrangling, visualization, and modeling skills to
+                  work. My fascination with <em>data exploration</em>,{" "}
+                  <em>regression analyis</em>, and <em>machine learning</em>{" "}
+                  inspire me to learn something new every day.
+                </p>
+                <p className="mb-2">
+                  Currently, I am finalizing my bachelor of science degree in
+                  economics as an international student at{" "}
+                  <a
+                    href="https://www.tilburguniversity.edu/education/bachelors-programs/economics"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:underline hover:decoration-gray-300 hover:decoration-2 hover:underline-offset-2"
+                  >
+                    Tilburg University
+                  </a>
+                  .
+                </p>
+                <p className="mb-10 sm:mb-0">
+                  Follow me on{" "}
+                  <a
+                    href="https://github.com/clemensheithecker"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:underline hover:decoration-gray-300 hover:decoration-2 hover:underline-offset-2"
+                  >
+                    GitHub
+                  </a>
+                  ,{" "}
+                  <a
+                    href="https://www.linkedin.com/in/clemensheithecker"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:underline hover:decoration-gray-300 hover:decoration-2 hover:underline-offset-2"
+                  >
+                    LinkedIn
+                  </a>{" "}
+                  and{" "}
+                  <a
+                    href="https://twitter.com/cheithecker"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:underline hover:decoration-gray-300 hover:decoration-2 hover:underline-offset-2"
+                  >
+                    Twitter
+                  </a>
+                  .
+                </p>
+              </div>
+            </div>
+            <div className="sm:col-span-6 md:col-span-5">
+              <Image
+                src={profilePicture}
+                alt="Picture of Clemens Heithecker"
+                layout="responsive"
+                placeholder="blur"
+              />
             </div>
           </div>
-          <div className="sm:col-span-6 md:col-span-5">
-            <Image
-              src={profilePicture}
-              alt="Picture of Clemens Heithecker"
-              layout="responsive"
-              placeholder="blur"
-            />
+        </div>
+        <div className="mt-4 px-5 py-5 sm:py-8 sm:px-11">
+          <h2 className="mb-4 font-serif text-2xl font-bold">Projects</h2>
+          <p className="mb-8">
+            Here is a selection of my recent projects in which I applied my data
+            science skills:
+          </p>
+          <div className="mb-8 grid grid-cols-1 gap-6 sm:grid-cols-2">
+            {projects.map((project, index) => (
+              <ProjectCard
+                key={index}
+                title={project.title}
+                description={project.description}
+                tags={project.tags}
+                link={project.link}
+                gradient={project.gradient}
+              />
+            ))}
           </div>
+          <p className="text-gray-600">
+            Find all my projects on my{" "}
+            <a
+              href="https://github.com/clemensheithecker"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline hover:decoration-gray-300 hover:decoration-2 hover:underline-offset-2"
+            >
+              GitHub page
+            </a>
+            .
+          </p>
         </div>
       </main>
     </>
