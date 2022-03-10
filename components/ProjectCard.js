7565bb3b33d7ad766da123158461e23e419d1614
Clemens S. Heithecker <clemens@heithecker.dev>
2022-03-10 09:05:12 +0100

    Update project card to align footer to the bottom

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,14 +1,22 @@
 import Link from "next/link";
 
-export default function Navitem({ title, description, tags, link, gradient }) {
+export default function ProjectCard({
+  title,
+  description,
+  tags,
+  link,
+  gradient,
+}) {
   return (
     <Link href={link}>
       <a
         className={`transform bg-gradient-to-r ${gradient} p-1 transition-all hover:scale-[1.01]`}
       >
-        <div className="h-full bg-white p-4 dark:bg-black">
-          <h3 className="mb-2 text-lg font-semibold">{title}</h3>
-          <p className="mb-4">{description}</p>
+        <div className="h-full flex-col justify-between bg-white p-4 dark:bg-black sm:flex">
+          <div>
+            <h3 className="mb-2 text-lg font-semibold">{title}</h3>
+            <p className="mb-4">{description}</p>
+          </div>
           <ul className="flex space-x-3">
             {tags.map((tag, index) => (
               <li
