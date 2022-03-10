491c18c110963b5b713565b5abfc07a04a82dc8e
Clemens S. Heithecker <clemens@heithecker.dev>
2022-03-10 09:45:25 +0100

    Add Open Graph image meta tag

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -27,6 +27,7 @@ export default function ProjectPage({
   mdxSource,
 }) {
   const coverImage = require(`/public/images/projects/${slug}.png`);
+  const coverImageURL = `https://clemensheithecker.com/images/projects/${slug}.png`;
 
   const data = {
     description: description,
@@ -46,6 +47,7 @@ export default function ProjectPage({
           property="og:url"
           content={`https://clemensheithecker.com/projects/${slug}`}
         />
+        <meta property="og:image" content={coverImageURL} />
         <meta property="og:type" content="article" />
       </Head>
       <article className="prose-t prose prose-bw mx-auto w-full max-w-screen-lg py-5 px-5 prose-h1:font-serif prose-h2:font-serif prose-a:break-words prose-a:no-underline hover:prose-a:underline hover:prose-a:decoration-2 hover:prose-a:underline-offset-2 prose-code:rounded-md prose-code:bg-gray-100 prose-code:py-1 prose-code:px-2 prose-code:font-normal prose-code:before:content-none prose-code:after:content-none prose-pre:rounded-none prose-ul:list-[square] prose-table:mx-auto dark:prose-invert dark:prose-code:bg-gray-900 sm:py-8 sm:px-11 sm:prose-table:w-5/6 md:prose-table:w-4/6">
