08f3db315b4b49d2a163e6b0357c5057e1a573f3
Clemens S. Heithecker <clemens@heithecker.dev>
2022-04-02 14:37:55 +0200

    Update project card design

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -12,16 +12,16 @@ export default function ProjectCard({
       <a
         className={`transform bg-gradient-to-r ${gradient} p-1 transition-all hover:scale-[1.01]`}
       >
-        <div className="h-full flex-col justify-between bg-white p-4 dark:bg-black sm:flex">
+        <div className="h-full flex-col justify-between bg-white p-4 opacity-[97.5%] dark:bg-black dark:opacity-[92.5%] sm:flex">
           <div>
             <h3 className="mb-2 text-lg font-semibold">{title}</h3>
-            <p className="mb-4">{description}</p>
+            <p className="mb-8">{description}</p>
           </div>
           <ul className="flex space-x-3">
             {tags.map((tag, index) => (
               <li
                 key={index}
-                className="rounded-lg bg-gray-100 px-2 py-1 text-sm text-gray-600 dark:bg-gray-800 dark:text-gray-400"
+                className="rounded-lg bg-gray-100 px-2 py-1 text-sm text-gray-700 opacity-90 dark:bg-gray-800 dark:text-gray-300"
               >
                 {tag}
               </li>
