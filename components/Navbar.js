6636be7e12470d5805944fe5115c032bc582f321
Clemens S. Heithecker <clemens@heithecker.dev>
2022-04-03 12:42:03 +0200

    Update navbar style based on scroll behavior

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
 import Navitem from "./Navitem";
 
 export default function Navbar() {
+  // Calculate client window height and store it as a state
+  const [clientWindowHeight, setClientWindowHeight] = useState();
+
+  const handleScroll = () => {
+    setClientWindowHeight(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
+
+  /*
+  useEffect(() => {
+    console.log(`Client window height = ${clientWindowHeight}`);
+  }, [clientWindowHeight]);
+  */
+
   return (
-    <nav className="fixed top-0 left-0 z-10 w-full bg-white bg-opacity-80 backdrop-blur-xl backdrop-filter firefox:bg-opacity-100 dark:bg-black dark:bg-opacity-80 dark:firefox:bg-opacity-100">
+    // Change navbar style based on client window height
+    <nav
+      className={`fixed top-0 left-0 z-10 w-full bg-white bg-opacity-80 backdrop-blur-xl backdrop-filter firefox:bg-opacity-100 dark:bg-black dark:bg-opacity-80 dark:firefox:bg-opacity-100 ${
+        clientWindowHeight > 0
+          ? `shadow-sm ring-1 ring-gray-900 ring-opacity-5`
+          : ``
+      }`}
+    >
       <div className="mx-auto max-w-screen-lg px-2 py-4 sm:px-8">
         <div className="flex items-center justify-between">
           <div className="flex space-x-2">
