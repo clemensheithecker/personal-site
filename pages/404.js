1dea7e7595857e13661cd63bfb661aa4cc195e73
Clemens S. Heithecker <clemens@heithecker.dev>
2022-04-04 00:45:13 +0200

    Change opacity of 404 message

--- pages/404.js
+++ pages/404.js
@@ -29,10 +29,10 @@ export default function Custom404() {
       </Head>
       <main className="mx-auto flex max-w-screen-lg items-center justify-center px-5 py-5 sm:min-h-[calc(100vh-68px)] sm:py-8 sm:px-11">
         <div className="sm:relative sm:max-w-xl">
-          <div className="absolute -top-24 -left-24 hidden h-96 w-96 animate-blob select-none rounded-full bg-rose-400 opacity-20 mix-blend-multiply blur-2xl filter ease-in-out motion-reduce:scale-90 motion-reduce:animate-none dark:mix-blend-normal sm:block"></div>
-          <div className="animation-delay-2000 absolute -top-24 -right-24 hidden h-96 w-96 animate-blob select-none rounded-full bg-purple-400 opacity-20 mix-blend-multiply blur-2xl filter ease-in-out motion-reduce:scale-110 motion-reduce:animate-none dark:mix-blend-normal sm:block"></div>
-          <div className="animation-delay-4000 absolute top-0 left-24 hidden h-96 w-96 animate-blob select-none rounded-full bg-indigo-400 opacity-20 mix-blend-multiply blur-2xl filter ease-in-out motion-reduce:animate-none dark:mix-blend-normal sm:block"></div>
-          <div className="bg-white opacity-80 dark:bg-black sm:relative sm:-m-8 sm:p-8">
+          <div className="absolute -top-24 -left-24 h-96 w-96 animate-blob select-none overflow-hidden rounded-full bg-rose-400 opacity-20 mix-blend-multiply blur-2xl filter ease-in-out motion-reduce:scale-90 motion-reduce:animate-none dark:mix-blend-normal sm:block"></div>
+          <div className="animation-delay-2000 absolute -top-24 -right-24 h-96 w-96 animate-blob select-none overflow-hidden rounded-full bg-purple-400 opacity-20 mix-blend-multiply blur-2xl filter ease-in-out motion-reduce:scale-110 motion-reduce:animate-none dark:mix-blend-normal sm:block"></div>
+          <div className="animation-delay-4000 absolute top-0 left-24 h-96  w-96 animate-blob select-none overflow-hidden rounded-full bg-indigo-400 opacity-20 mix-blend-multiply blur-2xl filter ease-in-out motion-reduce:animate-none dark:mix-blend-normal sm:block"></div>
+          <div className="-mx-5 -my-6 h-96 bg-white py-6 px-5 opacity-80 dark:bg-black sm:relative sm:-m-8 sm:h-auto sm:p-8">
             <h1 className="mb-4 font-serif text-4xl font-bold">
               The page you're looking for can't be found.
             </h1>
