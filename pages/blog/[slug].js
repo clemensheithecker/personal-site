f2545866167a42e2f6fea9d9cb48d3530cdaf721
Clemens S. Heithecker <clemens@heithecker.dev>
2022-04-05 01:14:26 +0200

    Fix blog page transition to include date

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -26,24 +26,24 @@ export default function PostPage({
         />
         <meta property="og:type" content="article" />
       </Head>
-      <article className="w-scren mx-auto w-full max-w-screen-lg py-5 px-5 sm:py-8 sm:px-11">
+      <motion.article
+        className="w-scren mx-auto w-full max-w-screen-lg py-5 px-5 sm:py-8 sm:px-11"
+        initial={{ opacity: 0.75, y: 6 }}
+        animate={{
+          opacity: 1,
+          y: 0,
+          transition: { duration: 0.4, ease: [0, 0, 0.2, 1] },
+        }}
+      >
         <p className="text-gray-600 dark:text-gray-400">
           {updated == null
             ? `Published on ${moment(date).format("LL")}`
             : `Last updated on ${moment(updated).format("LL")}`}
         </p>
-        <motion.div
-          className="prose prose-bw max-w-screen-lg prose-h1:my-6 prose-h1:font-serif prose-h1:text-4xl prose-h1:font-bold prose-h2:mb-4 prose-h2:mt-12 prose-h2:font-serif prose-p:mb-6 prose-p:leading-normal last-of-type:prose-p:mb-0 prose-a:break-words prose-a:no-underline hover:prose-a:underline hover:prose-a:decoration-2 hover:prose-a:underline-offset-2 prose-code:rounded-md prose-code:bg-gray-100 prose-code:py-1 prose-code:px-2 prose-code:font-normal prose-code:before:content-none prose-code:after:content-none prose-pre:rounded-none prose-ul:list-[square] prose-li:leading-normal prose-table:mx-auto dark:prose-invert dark:prose-code:bg-gray-900 sm:prose-table:w-5/6 md:prose-table:w-4/6"
-          initial={{ opacity: 0.75, y: 6 }}
-          animate={{
-            opacity: 1,
-            y: 0,
-            transition: { duration: 0.4, ease: [0, 0, 0.2, 1] },
-          }}
-        >
+        <div className="prose prose-bw max-w-screen-lg prose-h1:my-6 prose-h1:font-serif prose-h1:text-4xl prose-h1:font-bold prose-h2:mb-4 prose-h2:mt-12 prose-h2:font-serif prose-p:mb-6 prose-p:leading-normal last-of-type:prose-p:mb-0 prose-a:break-words prose-a:no-underline hover:prose-a:underline hover:prose-a:decoration-2 hover:prose-a:underline-offset-2 prose-code:rounded-md prose-code:bg-gray-100 prose-code:py-1 prose-code:px-2 prose-code:font-normal prose-code:before:content-none prose-code:after:content-none prose-pre:rounded-none prose-ul:list-[square] prose-li:leading-normal prose-table:mx-auto dark:prose-invert dark:prose-code:bg-gray-900 sm:prose-table:w-5/6 md:prose-table:w-4/6">
           <MDXRemote {...mdxSource} />
-        </motion.div>
-      </article>
+        </div>
+      </motion.article>
     </>
   );
 }
